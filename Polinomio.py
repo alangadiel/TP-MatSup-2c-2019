e3b5e188bd6bf804a2d6b2ae2abd41c1ac2ea57b
Nicosimi <nicosimi993@gmail.com>
2019-10-25 01:36:59 -0300

    Faltan testear algunos casos

--- Polinomio.py
+++ Polinomio.py
@@ -1,12 +1,29 @@
 class Polinomio():
-	def __init__(coeficientes, self):
+	def __init__(self, coeficientes):
 		"""Crea un polinomio a partir de una lista con sus coeficientes
 		[1,2,3] => 1+2x+3x**2
 		"""
-		self.coefs = coeficientes
+		self.coefs = list(coeficientes)
 
+	def __str__(self):
+		res = ""
+		if len(self.coefs)==0:
+			return res
+		else:
+			for i in range(0,len(self.coefs)):
+				if i==0:
+					res += str(self.coefs[i])
+				else:
+					res += str(self.coefs[i]) + "x^{}".format(i)
+				if i!=len(self.coefs)-1:
+					res += " + "
+			return res
+	
+	def __len__(self):
+		return len(self.coefs)
+	
 	def grado(self):
-		return self.coefs.len()-1
+		return len(self.coefs)-1
 		
 	def evaluar(self, x):
 		res = 0
@@ -19,31 +36,35 @@ class Polinomio():
 		coef_p1 = self.coefs
 		coef_p2 = otro.coefs
 		#expando la lista de coeficientes hasta que igualen al grado mas alto
-		if length(coef_p1)!=length(coef_p2):
-			if length(coef_p1)>length(coef_p2):
-				coef_p2 = coef_p2 + [0]*(length(coef_p1) - length(coef_p2))
+		if len(coef_p1)!=len(coef_p2):
+			if len(coef_p1)>len(coef_p2):
+				coef_p2 = coef_p2 + [0]*(len(coef_p1) - len(coef_p2))
 			else:
-				coef_p1 = coef_p1 + [0]*(length(coef_p1) - length(coef_p1))
-		res = Polinomio([0]*length(coef_p1))
-		for i in range(0, length(res)):
+				coef_p1 = coef_p1 + [0]*(len(coef_p2) - len(coef_p1))
+		res = Polinomio([0]*len(coef_p1))
+		for i in range(0, len(res)):
 			res.coefs[i] = res.coefs[i] + coef_p1[i] + coef_p2[i]
 		return res
 	
 	def aumentargrado(self, i):
-		pol = Polinomio([0]*length(self.coef))
+		pol = Polinomio([0]*len(self.coefs))
 		pol = pol + self
-		pol.coef = [0]*i + pol.coef
+		pol.coefs = [0]*i + pol.coefs
 		return pol
 	
 	def multesc(self, k):
-		pol = Polinomio(self.coef)
-		for i in self.coef:
-			i = i*k
+		pol = Polinomio(self.coefs)
+		for i in range(0, len(pol.coefs)):
+			pol.coefs[i] = pol.coefs[i]*k
 		return pol
 	
-	def __mult__(self, otro):
+	def __mul__(self, otro):
 		"""Multiplica 2 polinomios y devuelve un polinomio resultado de la multiplicacion"""
 		pol = Polinomio([0])
-		for i in range(o, length(otro.coef)):
-			pol = pol + self.multesc(otro.coef[i]).aumentargrado(i)
+		for i in range(0, len(otro.coefs)):
+		#	print("Coeficiente: {}".format(otro.coefs[i]))
+		#	print("Parte {0} = {1}".format(i,self.multesc(otro.coefs[i]).aumentargrado(i)))
+			pol = pol + self.multesc(otro.coefs[i]).aumentargrado(i)
+		#	print("Polinomio: {}".format(pol))
+			
 		return pol
