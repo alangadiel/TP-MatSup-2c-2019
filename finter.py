fa31f352d7d4160465553986e05c40c3ffc71c94
emiortiz <emiliano.ortiz@hotmail.com>
2019-11-06 12:55:47 -0300

    Tabla funcionando para agregar datos

--- finter.py
+++ finter.py
@@ -2,11 +2,14 @@ import sys
 import re
 import math
 from PyQt5 import uic, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QTableWidgetItem, QLineEdit, QMessageBox, QAbstractItemView
+
+modo = ["Lagrange","NG Progresivo", "NG Regresivo"]
+
+modoSeleccionado = 0
+puntos = []
 
 qtCreatorFile = "interface.ui" # Nombre del archivo aquí
-
-puntos = []
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
@@ -21,13 +24,42 @@ class FINTER(QtWidgets.QMainWindow, Ui_MainWindow):
         self.mostrarPasos.clicked.connect(self.mostrarPasos_app)
         self.cambiarModo.clicked.connect(self.cambiarModo_app)
         self.finalizar.clicked.connect(self.finalizar_app)
-
-
+        self.quitar.clicked.connect(self.quitar_app)
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnWidth(0, 106)
+        self.tableWidget.setColumnWidth(1, 106)
+        self.tableWidget.setHorizontalHeaderLabels(("x","F(x)"))
+        # Deshabilitar edición
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # Deshabilitar el comportamiento de arrastrar y soltar
+        self.tableWidget.setDragDropOverwriteMode(False)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.modoInterpolacion.setText(modo[modoSeleccionado])
     def agregar_app(self):
         punto = QInputDialog.getInt(self, "Agregar punto","Numero:", 0, 0, 100000, 1)    
         if punto[1]:
             puntos.append(punto[0])
+            row = len(puntos)
+            self.tableWidget.setRowCount(row-1)
+            x = QTableWidgetItem(str(row))
+            y = QTableWidgetItem(str(punto[0]))
+            self.tableWidget.insertRow(row-1)
+            self.tableWidget.setItem(row-1, 0, x)
+            self.tableWidget.setItem(row-1, 1, y)
             print(puntos)
+
+    def quitar_app(self):
+        if len(puntos) != 0:
+            puntos.pop()
+            row = len(puntos)
+            self.tableWidget.removeRow(row)
+            self.tableWidget.setRowCount(row)
+        else:
+            QMessageBox.about(self, "Mensaje","La lista esta vacia")
 
     def mostrarPasos_app(self):
         QMessageBox.about(self, "Punto de especializacion", "El punto es")
