acb35dc82e8abd6e1b59d595269d51199c1cab03
emiortiz <emiliano.ortiz@hotmail.com>
2019-11-07 16:41:34 -0300

    Funciones de interpolacion terminadas

--- finter.py
+++ finter.py
@@ -10,7 +10,7 @@ import sympy as sym
 
 modo = ["Lagrange","NG Progresivo", "NG Regresivo"]
 
-modoSeleccionado = 0
+modoSeleccionado = 1
 
 mostrarPasos = []
 
@@ -32,7 +32,7 @@ class FINTER(QtWidgets.QMainWindow, Ui_MainWindow):
         self.especializarPunto.clicked.connect(self.especializarPunto_app)
         self.mostrarPasos.clicked.connect(self.mostrarPasos_app)
         self.cambiarModo.clicked.connect(self.cambiarModo_app)
-        self.finalizar.clicked.connect(self.interpolacion_lagrange)
+        self.finalizar.clicked.connect(self.interpolacion_NGReg)
         self.quitar.clicked.connect(self.quitar_app)
         self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.tableWidget.setSelectionMode(QAbstractItemView.SingleSelection)
@@ -53,8 +53,8 @@ class FINTER(QtWidgets.QMainWindow, Ui_MainWindow):
         x = QInputDialog.getInt(self, "Agregar punto","Punto x:", 0, 0, 100000, 1)  
         y = QInputDialog.getInt(self, "Agregar punto","Punto y:", 0, 0, 100000, 1)    
         if x and y:
-            xi.append(x[0])
-            yi.append(y[0])
+            xi.append(float(x[0]))
+            yi.append(float(y[0]))
             row = len(xi)
             self.tableWidget.setRowCount(row-1)
             punto = QTableWidgetItem(str(x[0]))
@@ -74,10 +74,25 @@ class FINTER(QtWidgets.QMainWindow, Ui_MainWindow):
             QMessageBox.about(self, "Mensaje","La lista esta vacia")
 
     def mostrarPasos_app(self):
+        if modoSeleccionado == 0:
+            self.mostrarLagrange()
+        else: 
+            self.mostrarNGP()
+
+
+    def mostrarLagrange(self):
         string = ""
         for i in range(len(mostrarPasos)):        
             string += "L" + str(i) + ": " + str(mostrarPasos[i]) + "\n"
-        QMessageBox.about(self, "Punto de especializacion", string)
+        QMessageBox.about(self, "Mostrar Pasos", string)
+    
+    def mostrarNGP(self):
+        string = ""
+        print(mostrarPasos)
+        for i in range(len(mostrarPasos[0])):        
+            string += "a" + str(i) + ": " + str(mostrarPasos[0][i]) + "\n"
+        QMessageBox.about(self, "mostrarPasos", string)
+    
 
     def especializarPunto_app(self):
         punto, okPressed = QInputDialog.getText(self, "Especializar en un punto","Punto:", QLineEdit.Normal, "")
@@ -113,10 +128,88 @@ class FINTER(QtWidgets.QMainWindow, Ui_MainWindow):
         especializacionEnPunto = sym.lambdify(x,polinomio)
     
         
-    #def interpolacion_NGProg(self):
-    
-    #def interpolacion_NGReg(self):
-    
+    def interpolacion_NGReg(self):
+        n = len(xi)
+        x = sym.Symbol('x')
+        a = []
+        termino = yi
+        #Inicialzo a con los valores de la imagen(yi)
+        for i in range(n):
+            a.append(yi[i])
+
+        #Recorre los puntos yi generando las diferencias finitas de orden 1
+        #Una vez generadas las diferencias finitas las pongo en el vector a 
+        #de adelante hacia atras dejando el valor el valor de a que necesito para
+        #generar el polinomio 
+        #La ultima posicicon del vector a eliminando el valor de yi que no necesito mas
+        for j in range(1, n):
+            #Hago las diferencia finita entre yi y yi+1
+            for i in range(n-1, j-1, -1):
+                a[i] = float(a[i]-a[i-1])/float(xi[i]-xi[i-j])
+        #Pongo en la primera posicion de el vector a el valor de yi que me importa para
+        #generar el polinomio 
+        a[0] = yi[n-1]
+        mostrarPasos.append(a)
+
+        #Genero el polinomio 
+        polinomio = a[0]
+        termino = 1
+
+        #Recorre el vector de bn y los multiplica por sus (x - xn) pertenecientes
+        for h in range (1, len(a), 1):
+            #Genero el termino y despues lo sumo
+            for k in range(0, h, 1):
+                termino *= (x-xi[n-k-1])
+            termino *= a[h]
+            polinomio += termino
+            #Lo inicializo para la proxima pasada 
+            termino = 1
+            
+        polinomio = polinomio.expand()
+        # Escribo el polinomio y pongo la funcion para la especializacion de un punto     
+        self.polinomio.setText(str(polinomio.expand()))
+        especializacionEnPunto = sym.lambdify(x,polinomio)
+
+    def interpolacion_NGProg(self):
+        n = len(xi)
+        x = sym.Symbol('x')
+        a = []
+        termino = yi
+        #Inicializo a con los valores de la imagen(yi)
+        for i in range(n):
+            a.append(yi[i])
+
+        #Recorro los valores de la imagen para generar las diferencias finitas de orden 1
+        #Y los agrego las diferencias finitas en el vector a eliminando los valores de la imagen 
+        #Que ya no me sirven, de adelante hacia atras. Y vuelvo a iterar haciendo lo mismo (cantidad de puntos -1 ) veces
+        for j in range(1, n):
+            #Aca hago las diferencias finitas 
+            for i in range(0, n-j, 1):
+                a[i] = float(a[i+1]-a[i])/float(xi[i+j]-xi[i])
+        #Le agrego el unico valor de la imagen que me interesa en la ultima posicion del vector
+        a[n-1] = yi[0]
+        print(a)
+        mostrarPasos.append(a)
+
+        #Genero el polinomio 
+        polinomio = a[n-1]
+        termino = 1
+        #Recorro el vector a tantas veces como elementos tenga
+        for h in range (1, len(a), 1):
+            #Dependiendo de cual es la posicion de el elemento
+            #Lo multiplico por (x-xn)
+            for k in range(0, h, 1):
+                termino *= (x-xi[k])
+            termino *= a[n-h-1]
+            print(termino)
+            #Le sumo el termino al polinomio
+            polinomio += termino
+            #Lo inicializo para la proxima vuelta
+            termino = 1
+        polinomio = polinomio.expand()
+        #Seteo la info en el el cuadro de texto del polinomio y pongo la funcion para especializar el punto
+        self.polinomio.setText(str(polinomio.expand()))
+        especializacionEnPunto = sym.lambdify(x,polinomio)
 
 if __name__ == "__main__":
     app =  QtWidgets.QApplication(sys.argv)
